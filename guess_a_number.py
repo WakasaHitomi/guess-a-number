bf1b7c53413c66ac916c0f50ecc23a8f3f757f71
Jennings <djenni4049@greenville.k12.sc.us>
2017-09-14 09:28:49 -0400

    Limit adjustment

--- guess_a_number.py
+++ guess_a_number.py
@@ -3,12 +3,17 @@ import random
 #config
 low = 1
 high = 100
+limit = 10
+
+# start game
 rand = random.randint(low,high)
 print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
 
 guess = -1
+tries = 0
 
-while guess != rand:
+# play the game
+while guess != rand and tries < limit:
     guess = input("Take a guess: ")
     guess = int(guess)
     
@@ -16,7 +21,13 @@ while guess != rand:
         print("You guessed too low.")
     elif guess > rand:
         print("You guessed too high.")
-    else:
-        print("You got it!")
 
-print("Game over")
+    tries += 1
+
+if guess == rand:
+    print("Great Job!")
+else:
+        print("No, you never fail to disappoint do you? The correct answer was " + str(rand) + ".")
+        
+
+
